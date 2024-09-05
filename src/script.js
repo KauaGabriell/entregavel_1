f19f3994fa8d5c34eeebce56351986047c3eac1c
Kauã Gabriell <kauagabriell64@gmail.com>
2024-09-05 14:44:04 -0300

    Entregável 01.

--- src/script.js
+++ src/script.js
@@ -2,7 +2,41 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //ESCREVA SEU CÓDIGO AQUI!!
+let resultado = 0
+let num1 = parseInt(prompt("Digite um número: "))
+let num2 = parseInt(prompt("Digite outro número: "))
+let operador = prompt("Digite a operação a ser realizada: ")
 
+function Calculadora(num1, num2, operador){
+  switch(operador){
+    case "+":
+      resultado = num1 + num2
+      break
+    case "-":
+      resultado = num1 - num2
+      break
+    case "*":
+      resultado = num1 * num2
+      break
+    case "/":
+      resultado = num1 / num2
+      break
+    case "e":
+      let contador = 1
+      resultado = num1 * num2
+      while(contador < num2 - 1){
+        resultado = resultado * num1
+        contador++
+      }
+      break
+    
+  }
+
+  if(resultado == undefined || resultado > 1000000){
+    console.log("ERRO")
+  }
+  return resultado
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
